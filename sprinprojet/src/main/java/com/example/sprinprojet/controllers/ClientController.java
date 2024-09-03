8ba898659035c2b96b88b2d145b2ba4e4ac3138a
Salma Lamkhantar <72557122+salma-lam@users.noreply.github.com>
2024-09-03 22:45:46 +0200

    Update ClientController.java

--- sprinprojet/src/main/java/com/example/sprinprojet/controllers/ClientController.java
+++ sprinprojet/src/main/java/com/example/sprinprojet/controllers/ClientController.java
@@ -10,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+
 
 @Controller
 public class ClientController {
@@ -46,8 +48,41 @@ public class ClientController {
             return "register";
         }
         clientService.saveClient(client);
-        return "redirect:/login"; 
-    }
+        return "redirect:/login";
 
 
+
+        // Partie de l'agent
+
+        @GetMapping("/profileclient")
+        public String getlistClients(Model model, @RequestParam(value = "success", required = false) String success) {
+            List<Client> clientsList = clientService.getAllClients();
+            model.addAttribute("clients", clientsList);
+            model.addAttribute("success", success);
+            System.out.println("Nombre de clients est " + clientsList.size());
+            return "profileclient";
+        }
+    
+        @GetMapping("/profileclient/{idClient}")
+        public String editClientForm(@PathVariable Long idClient, Model model) {
+            model.addAttribute("client", clientService.getClientById(idClient));
+            return "profileclient";
+        }
+    
+        @PostMapping("/profileclient/{idClient}")
+        public String updateClient(@PathVariable Long idClient, @ModelAttribute("client") Client client) {
+            // get client from database by id
+            Client existingClient = clientService.getClientById(idClient);
+            existingClient.setNom(client.getNom());
+            existingClient.setPrenom(client.getPrenom());
+            existingClient.setEmail(client.getEmail());
+            existingClient.setTele(client.getTele());
+            existingClient.setAdresse(client.getAdresse());
+            existingClient.setGenre(client.getGenre());
+    
+            // save updated client object
+            clientService.updateClient(existingClient);
+            return "redirect:/profileclient?success=true";
+        }
+    
 }
