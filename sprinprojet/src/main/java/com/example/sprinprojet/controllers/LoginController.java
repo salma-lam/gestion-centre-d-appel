ecb31a6c3546e42def546e2b61157b1fb0e915f3
Salma Lamkhantar <72557122+salma-lam@users.noreply.github.com>
2024-09-24 17:46:13 +0200

    Update LoginController.java

--- sprinprojet/src/main/java/com/example/sprinprojet/controllers/LoginController.java
+++ sprinprojet/src/main/java/com/example/sprinprojet/controllers/LoginController.java
@@ -2,7 +2,6 @@ package com.example.sprinprojet.controllers;
 
 import com.example.sprinprojet.Repository.ClientRepository;
 import com.example.sprinprojet.model.Client;
-// import com.example.sprinprojet.service.ClientService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +16,7 @@ public class LoginController {
 
     @Autowired
     private ClientRepository clientRepository;
-    // @Autowired
-    // private ClientService clientService;
+
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
