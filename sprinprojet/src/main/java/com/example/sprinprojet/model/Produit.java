42af5e3e0acf0fd2cc9a2e65306fa41668f7beb0
Salma Lamkhantar <72557122+salma-lam@users.noreply.github.com>
2024-10-03 20:37:01 +0200

    Update Produit.java

--- sprinprojet/src/main/java/com/example/sprinprojet/model/Produit.java
+++ sprinprojet/src/main/java/com/example/sprinprojet/model/Produit.java
@@ -1,26 +1,26 @@
-package com.example.sprinprojet.model;
+// package com.example.sprinprojet.model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+// import jakarta.persistence.*;
+// import lombok.AllArgsConstructor;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "Produit")
-public class Produit {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long idProduit;
-    private String nomProduit;
-    private Float prixProduit;
-    private String categorieProduit;
-
-
-  //  @ManyToOne
-   // private Reclamation reclamations;
+// @Data
+// @AllArgsConstructor
+// @NoArgsConstructor
+// @Entity
+// @Table(name = "Produit")
+// public class Produit {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long idProduit;
+//     private String nomProduit;
+//     private Float prixProduit;
+//     private String categorieProduit;
 
 
-}
+//   //  @ManyToOne
+//    // private Reclamation reclamations;
+
+
+// }
